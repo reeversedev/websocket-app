833dc421c04370ccb3d6a13daf62a5edda4d4e83
Prateek Gogia <gogia.prateek@hotmail.com>
2017-12-23 16:51:58 +0530

    Uploaded the initial files.

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var socket = require('socket.io');
+// App setup
+var app = express();
+var server = app.listen(5000, function() {
+  console.log('Listening to request on 5000');
+});
+
+// Static files
+app.use(express.static('public'));
+
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket) {
+  console.log('Made a socket connection', socket.id);
+
+  socket.on('chat', function(data) {
+    io.sockets.emit('chat', data);
+  });
+  socket.on('typing', function(data) {
+    socket.broadcast.emit('typing', data);
+  })
+});
